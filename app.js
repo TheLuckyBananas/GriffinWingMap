ec65e7659245b6f3df32b1152d1ee17b1e37795f
TheLuckyBananas <stan@theluckybananas.me>
2026-07-08 19:40:17 -0500

    Loading app v24
    Bump app version for resource legend styling

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const INITIAL_ZOOM = 2;
 const MIN_ZOOM = INITIAL_ZOOM - 1;
 const MAX_ZOOM = INITIAL_ZOOM + 2;
 const MEMBER_BASE_LIMIT = 3;
-const APP_VERSION = "v23";
+const APP_VERSION = "v24";
 const VERSION_URL = "https://cdn.th.gl/dune-awakening/version.json";
 const SPICE_FIELDS_URL = "./deep-spice-fields.json?v=3";
 
